3b1f27caaa034c13f3b2faf37b26853b8c090bd2
rafaelb056 <rafaelbenitez@gmail.com>
2024-11-20 18:56:50 -0400

    add:file/por-referencia.php and por-valor.php

---
+++ tema-1/por-referecia.php
@@ -0,0 +1,10 @@
+<?php
+  function foo(&$var) {
+    $var++;
+  }
+
+  $a=5;
+  foo($a);
+  // $a es 6 aquí
+?>
+
---
+++ tema-1/por-valor.php
@@ -0,0 +1,5 @@
+<?php function suma($a, $b) { // Definición de la función y sus parámetros
+   $c = $a + $b;
+   return $c;  // Retorna el valor de la función
+}
+?>
