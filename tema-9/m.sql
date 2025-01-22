651724928ed4bd09714256f255b8b6ebb803c64f
rafaelb056 <rafaelbenitez056@gmail.com>
2025-01-21 20:19:59 -0400

    add:file/m.sql

--- tema-9/m.sql
+++ tema-9/m.sql
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS tema (
 
 CREATE TABLE IF NOT EXISTS respuesta (
   id INT NOT NULL AUTO_INCREMENT,
-  id_contenido INT NOT NULL,
+  id_tema INT NOT NULL,
   respuesta VARCHAR(100),
   autor VARCHAR(30),
   fecha DATE,
   PRIMARY KEY(id),
-  FOREIGN KEY(id_contenido) REFERENCES tema(id));
+  FOREIGN KEY(id_tema) REFERENCES tema(id));
 
 INSERT INTO tema (titulo, contenido, autor , fecha, hora) VALUES ('V.F. Corporation', 'Apparel', 'Johnna', '2024-01-30', '1:26:14'),
       ('Inter Parfums, Inc.', 'Package Goods/Cosmetics', 'Lorette', '2024-01-30', '15:38:04'),
@@ -24,7 +24,7 @@ INSERT INTO tema (titulo, contenido, autor , fecha, hora) VALUES ('V.F. Corporat
       ('ServisFirst Bancshares, Inc.', 'Major Banks', 'Tierney', '2024-09-29', '13:03:34'),
       ('ChromaDex Corporation', 'Specialty Chemicals', 'Benedetta', '2024-02-26', '12:12:22');
 
-INSERT INTO respuesta (id_contenido, respuesta, autor, fecha) VALUES ('1', 'Major Banks', 'Wilhelm', '2025-01-13'),
+INSERT INTO respuesta (id_tema, respuesta, autor, fecha) VALUES ('1', 'Major Banks', 'Wilhelm', '2025-01-13'),
     ('2', 'Biotechnology: Laboratory Analytical Instruments', 'Noemi', '2024-11-26'),
     ('3', 'Semiconductors', 'Bruno', '2024-10-05'),
     ('4', 'Major Pharmaceuticals', 'Kaine', '2024-11-17'),
