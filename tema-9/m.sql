c5b063141efa5e3b8043794d9b4fdd939d31e0af
aristides1000 <aristides1000@gmail.com>
2025-01-22 20:09:32 -0400

    Fix: identation

--- tema-9/m.sql
+++ tema-9/m.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS php2;
+
 USE php2;
+
 CREATE TABLE IF NOT EXISTS tema (
   id INT NOT NULL AUTO_INCREMENT,
   titulo VARCHAR(30),
@@ -7,7 +9,8 @@ CREATE TABLE IF NOT EXISTS tema (
   autor VARCHAR(30),
   fecha DATE,
   hora TIME,
-  PRIMARY KEY(id));
+  PRIMARY KEY(id)
+);
 
 CREATE TABLE IF NOT EXISTS respuesta (
   id INT NOT NULL AUTO_INCREMENT,
@@ -16,16 +19,19 @@ CREATE TABLE IF NOT EXISTS respuesta (
   autor VARCHAR(30),
   fecha DATE,
   PRIMARY KEY(id),
-  FOREIGN KEY(id_tema) REFERENCES tema(id));
+  FOREIGN KEY(id_tema) REFERENCES tema(id)
+);
 
-INSERT INTO tema (titulo, contenido, autor , fecha, hora) VALUES ('V.F. Corporation', 'Apparel', 'Johnna', '2024-01-30', '1:26:14'),
-      ('Inter Parfums, Inc.', 'Package Goods/Cosmetics', 'Lorette', '2024-01-30', '15:38:04'),
-      ('CNA Financial Corporation', 'Property-Casualty Insurers', 'Tomi', '2024-08-22', '5:34:41'),
-      ('ServisFirst Bancshares, Inc.', 'Major Banks', 'Tierney', '2024-09-29', '13:03:34'),
-      ('ChromaDex Corporation', 'Specialty Chemicals', 'Benedetta', '2024-02-26', '12:12:22');
+INSERT INTO tema (titulo, contenido, autor , fecha, hora)
+VALUES ('V.F. Corporation', 'Apparel', 'Johnna', '2024-01-30', '1:26:14'),
+('Inter Parfums, Inc.', 'Package Goods/Cosmetics', 'Lorette', '2024-01-30', '15:38:04'),
+('CNA Financial Corporation', 'Property-Casualty Insurers', 'Tomi', '2024-08-22', '5:34:41'),
+('ServisFirst Bancshares, Inc.', 'Major Banks', 'Tierney', '2024-09-29', '13:03:34'),
+('ChromaDex Corporation', 'Specialty Chemicals', 'Benedetta', '2024-02-26', '12:12:22');
 
-INSERT INTO respuesta (id_tema, respuesta, autor, fecha) VALUES ('1', 'Major Banks', 'Wilhelm', '2025-01-13'),
-    ('2', 'Biotechnology: Laboratory Analytical Instruments', 'Noemi', '2024-11-26'),
-    ('3', 'Semiconductors', 'Bruno', '2024-10-05'),
-    ('4', 'Major Pharmaceuticals', 'Kaine', '2024-11-17'),
-    ('5', 'Major Pharmaceuticals', 'Lionel', '2024-12-10');
+INSERT INTO respuesta (id_tema, respuesta, autor, fecha)
+VALUES ('1', 'Major Banks', 'Wilhelm', '2025-01-13'),
+('2', 'Biotechnology: Laboratory Analytical Instruments', 'Noemi', '2024-11-26'),
+('3', 'Semiconductors', 'Bruno', '2024-10-05'),
+('4', 'Major Pharmaceuticals', 'Kaine', '2024-11-17'),
+('5', 'Major Pharmaceuticals', 'Lionel', '2024-12-10');
--- tema-9/menu.php
+++ tema-9/menu.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./styles.css">
-  <title>Document</title>
-</head>
-<body>
-  <table>
-    <tr>
-      <td><a href="./foro.php">Foro</a></td>
-      <td><a href="./agregar_tema">Agregar Tema</a></td>
-    </tr>
-  </table>
-</body>
-</html>
+<table>
+  <tr>
+    <td><a href="./foro.php">Foro</a></td>
+    <td><a href="./agregar_tema">Agregar Tema</a></td>
+  </tr>
+</table>
